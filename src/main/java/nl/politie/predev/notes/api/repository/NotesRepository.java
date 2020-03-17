414a8a4a3e39288c4f0eda91d0b14f5ff5d4814a
Ruud Schiphorst <ruud.schiphorst2@politie.nl>
2020-03-17 13:35:15 +0100

    changed getall to test share

--- src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
+++ src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
@@ -38,7 +38,7 @@ public interface NotesRepository extends RefreshableRepository<Note, Long> {
 	void deleteByIdAndVersion(UUID noteID, Integer version);
 	
 	
-	@Query("SELECT n FROM Note n JOIN SharedNote s on n.noteID=s.noteID WHERE (n.owner =?1 OR s.sharedWithUsername= ?1 OR s.sharedWithGroupname IN (?2)) AND n.is_deleted = false AND s.isDeleted = false")
+	@Query("SELECT n FROM Note n LEFT JOIN SharedNote s on n.noteID=s.noteID WHERE (n.owner =?1 OR s.sharedWithUsername= ?1 OR s.sharedWithGroupname IN (?2)) AND n.is_deleted = false AND s.isDeleted = false")
 	List<Note> getAll(String username, String roles);
 
 	
