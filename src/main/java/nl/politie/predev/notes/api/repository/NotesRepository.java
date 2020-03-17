842ebe955e7762ab41a2bec3e5c01b055c55565b
Ruud Schiphorst <ruud.schiphorst2@politie.nl>
2020-03-17 13:43:27 +0100

    flipping syntax....

--- src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
+++ src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
@@ -38,7 +38,7 @@ public interface NotesRepository extends RefreshableRepository<Note, Long> {
 	void deleteByIdAndVersion(UUID noteID, Integer version);
 	
 	
-	@Query("SELECT n FROM Note n RIGHT JOIN SharedNote s on n.noteID=s.noteID WHERE (n.owner =?1 OR (s.sharedWithUsername= ?1 OR s.sharedWithGroupname IN (?2) AND s.isDeleted = false)) AND n.is_deleted = false")
+	@Query("SELECT n FROM Note n LEFT JOIN SharedNote s on n.noteID=s.noteID WHERE (n.owner =?1 OR (s.sharedWithUsername= ?1 OR s.sharedWithGroupname IN (?2) AND s.isDeleted = false)) AND n.is_deleted = false")
 	List<Note> getAll(String username, String roles);
 
 	
