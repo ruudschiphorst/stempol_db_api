3e0972f4f5345158eb94158496694e925778cec7
Ruud Schiphorst <ruud.schiphorst2@politie.nl>
2020-03-19 21:52:54 +0100

    Repo: username check to lower

--- src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
+++ src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
@@ -38,7 +38,7 @@ public interface NotesRepository extends RefreshableRepository<Note, Long> {
 	void deleteByIdAndVersion(UUID noteID, Integer version);
 	
 	
-	@Query("SELECT n FROM Note n LEFT JOIN SharedNote s on n.noteID=s.noteID WHERE (n.owner =?1 OR (s.sharedWithUsername= ?1 OR s.sharedWithGroupname IN (?2) AND s.isDeleted = false)) AND n.is_deleted = false ORDER BY n.generated_at DESC")
+	@Query("SELECT n FROM Note n LEFT JOIN SharedNote s on n.noteID=s.noteID WHERE (LOWER(n.owner) =LOWER(?1) OR (LOWER(s.sharedWithUsername)= LOWER(?1) OR LOWER(s.sharedWithGroupname) IN (LOWER(?2)) AND s.isDeleted = false)) AND n.is_deleted = false ORDER BY n.generated_at DESC")
 	List<Note> getAll(String username, String roles);
 
 	
