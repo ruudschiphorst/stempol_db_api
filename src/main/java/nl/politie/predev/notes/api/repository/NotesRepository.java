bd67a220ee855668749498070072120b9a1e732b
Ruud Schiphorst <ruud.schiphorst2@politie.nl>
2020-04-21 12:10:28 +0200

    My notes and Public notes endpoints fixed

--- src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
+++ src/main/java/nl/politie/predev/notes/api/repository/NotesRepository.java
@@ -44,7 +44,8 @@ public interface NotesRepository extends RefreshableRepository<Note, Long> {
 	@Query("SELECT n FROM Note n WHERE (LOWER(n.owner) =LOWER(?1) OR LOWER(n.created_by) = LOWER(?1)) AND n.is_deleted = false ORDER BY n.generated_at DESC")
 	List<Note> getMyNotes(String username, String roles);
 	
-	@Query("SELECT n FROM Note n WHERE n.is_public = true AND n.is_deleted = false ORDER BY n.generated_at DESC")
+//	@Query("SELECT n FROM Note n WHERE n.is_public = true AND n.is_deleted = false ORDER BY n.generated_at DESC")
+	@Query("SELECT n FROM Note n WHERE LOWER(n.owner) <> LOWER(?1) AND LOWER(n.created_by) <> LOWER(?1) AND n.is_deleted = false AND n.is_public = true ORDER BY n.generated_at DESC")
 	List<Note> getPublicNotes();
 	
 }
