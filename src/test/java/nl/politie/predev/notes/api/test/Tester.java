2734fd466731d4797971ef6eb56a4b1e174ede91
Ruud Schiphorst <ruud.schiphorst2@politie.nl>
2020-05-07 11:49:30 +0200

    secrets from env

--- src/test/java/nl/politie/predev/notes/api/test/Tester.java
+++ src/test/java/nl/politie/predev/notes/api/test/Tester.java
@@ -31,50 +31,50 @@ import nl.politie.predev.notes.api.util.RbacUtils;
 
 public class Tester {
 
-	@Test
-	public void sadas() throws IOException {
-		
-		String original = "/home/ruud/Downloads/eend.jpg";
-		String thumb ="/home/ruud/Downloads/thumb_eend.jpg";
-		
-		BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-		img.createGraphics().drawImage(ImageIO.read(new File(original)).getScaledInstance(100, 100, Image.SCALE_SMOOTH),0,0,null);
-		
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ImageIO.write( img, "jpg", baos );
-		baos.flush();
-		byte[] imageInByte = baos.toByteArray();
-		baos.close();
-		System.out.println(imageInByte.length);
-		
-		Files.write(Paths.get(thumb), imageInByte);
-		
-//		ImageIO.write(img, "jpg", new File(thumb));
-		
-	}
-	
-	@Test
-	public void henk() throws JsonProcessingException {
-	
-		
-		Note note = new Note();
-		note.setCreated_by("ruud");
-		note.setTitle("een titel");
-		note.setIs_deleted(false);
-		note.setIs_public(true);
-		note.setOwner("Ruud");
-		note.setVersion(1);
-		
-		ObjectMapper om = new ObjectMapper();
-		System.out.println( om.writeValueAsString(note));
-		
-	
-		
+//	@Test
+//	public void sadas() throws IOException {
 //		
-//		LocalDateTime d = LocalDateTime.parse ("2020-02-11 14:10:59.322672");
-//		System.out.println(d);
-		
-	}
+//		String original = "/home/ruud/Downloads/eend.jpg";
+//		String thumb ="/home/ruud/Downloads/thumb_eend.jpg";
+//		
+//		BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+//		img.createGraphics().drawImage(ImageIO.read(new File(original)).getScaledInstance(100, 100, Image.SCALE_SMOOTH),0,0,null);
+//		
+//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		ImageIO.write( img, "jpg", baos );
+//		baos.flush();
+//		byte[] imageInByte = baos.toByteArray();
+//		baos.close();
+//		System.out.println(imageInByte.length);
+//		
+//		Files.write(Paths.get(thumb), imageInByte);
+//		
+////		ImageIO.write(img, "jpg", new File(thumb));
+//		
+//	}
+//	
+//	@Test
+//	public void henk() throws JsonProcessingException {
+//	
+//		
+//		Note note = new Note();
+//		note.setCreated_by("ruud");
+//		note.setTitle("een titel");
+//		note.setIs_deleted(false);
+//		note.setIs_public(true);
+//		note.setOwner("Ruud");
+//		note.setVersion(1);
+//		
+//		ObjectMapper om = new ObjectMapper();
+//		System.out.println( om.writeValueAsString(note));
+//		
+//	
+//		
+////		
+////		LocalDateTime d = LocalDateTime.parse ("2020-02-11 14:10:59.322672");
+////		System.out.println(d);
+//		
+//	}
 	
 //	@Test
 //	public void blaat() throws IOException {
